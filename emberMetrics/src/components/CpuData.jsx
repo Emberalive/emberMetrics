4003b5169b615bb42f3fe2f56576b2020812fc80
Sammy Boy <samueltsutcliffe@gmail.com>
2025-08-14 00:45:48 +0100

    made the cpu percentage use be two colums instead of one

--- emberMetrics/src/components/CpuData.jsx
+++ emberMetrics/src/components/CpuData.jsx
@@ -1,69 +1,40 @@
-import {useState} from "react";
-
 export default function CpuData (props) {
     const cpuUsage = props.metrics.cpuUsage
-
-    let first = []
-    let second = []
-
-    const [isLong, setIsLong] = useState(false)
 
     function renderCpuUsage(cpuList) {
         return cpuList.map((core) => {
             return (
-                <li
-                    key={core.no} // always add a key in map!
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        padding: "10px",
-                        borderRadius: "5px",
-                        border: "1px solid",
-                        margin: "5px",
-                    }}
-                >
-                    <h1>Core - {core.no}</h1>
-                    <h2>{core.usage}%</h2>
+                <li style={{
+                    listStyleType: 'none',
+                    paddingLeft: '10px',
+                    border: '1px solid black',
+                    borderRadius: '10px',
+                    display: 'flex',
+                }}
+                    key={core.no}>
+                    <p>Core - {core.no}</p>
+                    <p>{core.usage}%</p>
                 </li>
             );
         });
     }
 
-
     try {
         let cpuUsagePercents = []
-        if (!cpuUsage || !Array.isArray(cpuUsage.cores)) {
-            throw new Error('Invalid CPU Usage');
-        }
 
-        if (cpuUsage.cores.length > 7) {
-            setIsLong(prev => !prev );
-            const firstHalf = cpuUsage.cores.slice(0, 6);
-            const secondHalf = cpuUsage.cores.slice(6);
-            first =  renderCpuUsage(firstHalf);
-            second = renderCpuUsage(secondHalf);
-
-        } else {
-            cpuUsagePercents = renderCpuUsage(cpuUsage.cores)
-        }
+        cpuUsagePercents = renderCpuUsage(cpuUsage.cores)
 
         return (
             <section style={{}}>
-                <h1>Total {cpuUsage.total}</h1>
-                {isLong ? <>
-                        <ul>
-                            {first}
-                        </ul>
-                        <ul>
-                            {second}
-                        </ul>
+                <h1>Total {cpuUsage.total.toFixed(2)}%</h1>
 
-                    </>
-                    : <ul>
+                <ul style={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(2, 1fr)",
+                    gap: "10px",
+                }}>
                     {cpuUsagePercents}
-                </ul>}
+                </ul>
             </section>
         )
     } catch (err) {
