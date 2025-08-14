aedc215a2c2e5d015cb53cd9876a96ec001543ff
sam-book <sanueltsutcliffe@gmail.com>
2025-08-14 14:51:11 +0100

    I have added a percent sign on the mempory usage section

--- emberMetrics/src/components/MemoryData.jsx
+++ emberMetrics/src/components/MemoryData.jsx
@@ -4,11 +4,11 @@ export default function MemoryData(props) {
         <section className="memory-info">
             <div>
                 <h2>Memory used - </h2>
-                <h3>{memoryUsage.usage}</h3>
+                <h3>{memoryUsage.usage}%</h3>
             </div>
             <div>
                 <h2>Memory Available - </h2>
-                <h3>{memoryUsage.available}</h3>
+                <h3>{memoryUsage.available}%</h3>
             </div>
         </section>
     )
