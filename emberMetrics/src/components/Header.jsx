d3bd58d464ef1a836f3ee8e220010823f19f24cb
sam-book <samueltsutcliffe@gmail.com>
2025-09-03 16:31:22 +0100

    I have fixed the header to fit in mobile view however there is an issue with it, but it fits, but i havent fixed the gap issues

--- emberMetrics/src/components/Header.jsx
+++ emberMetrics/src/components/Header.jsx
@@ -2,13 +2,15 @@ export default function Header (props) {
     const metrics = props.metrics
     return (
         <header className="header">
-            <h1>EmberMetrics</h1>
-            {(metrics && props.activeView === "resources") && <div>
-                <h2>Device:</h2>
-                <p style={{
-                    borderBottom: '1px solid var(--border-color)',
-                }}    >{metrics.hostName}</p>
-            </div>}
+            <div className="header__details" style={{display: 'flex', gap: '50px'}}>
+                <h1>EmberMetrics</h1>
+                {(metrics && props.activeView === "resources") && <div className="header__device-name">
+                    <h2>Device:</h2>
+                    <p style={{
+                        borderBottom: '1px solid var(--border-color)',
+                    }}    >{metrics.hostName}</p>
+                </div>}
+            </div>
             <button className="settings-button general-button" onClick={() => {
                 props.setActiveView("settings")
                 props.activeView === "settings" ? props.setActiveView("resources") : props.setActiveView("settings")
