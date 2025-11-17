3a79b1f8362c42d5b8083014cf3da39119772e74
Sammy Boy <samueltsutcliffe@gmail.com>
2025-11-17 18:12:29 +0000

    I have fixed the ember metrics remote device api that will send a readme as well as teh remote device script when click on a button

--- metrics-api/remote-device.js
+++ metrics-api/remote-device.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const app = express();
 const http = require('http');
-
 const fs = require('fs');
-
+const cors = require('cors');
 const path = require('path');
 
-const RDscript = './files/RDScript.js';
+const port = 3001
+
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'HEAD', 'OPTIONS'],
+    }
+));
+
+app.get("/", (req, res) => {
+    const fileName = path.join(__dirname, 'files/remote-device/remote-device.zip');
+
+    res.download(fileName, 'remote-device.zip');
+});
+
+app.listen(port, () => {
+    console.log(`[Server] API Listening on port ${port}`)
+});
