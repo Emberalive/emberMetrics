969bfac0db78fc8c4e693d7d8f4a7d92873086be
Sammy Boy <samueltsutcliffe@gmail.com>
2025-12-11 03:45:12 +0000

    I have updated the server so that when an api call goes out to the remoteDevice.js server, it does not send an etag, and does not cache the file, meaning it will download it everytime it is requested on the same browser

--- metrics-api/remote-device.js
+++ metrics-api/remote-device.js
@@ -9,15 +9,24 @@ const port = 3001
 
 app.use(cors(
     {
-        origin: '*',
+        origin: 'http://localhost:5173',
         methods: ['GET', 'HEAD', 'OPTIONS'],
     }
 ));
 
+app.disable("etag");
+
 app.get("/", (req, res) => {
     const fileName = path.join(__dirname, 'files/remote-device/remote-device.zip');
     console.log("[Server - /] - Sent a remote device to a user");
-    res.download(fileName, 'remote-device.zip');
+    res.set({
+        "Content-Type": "application/zip",
+        "Content-Disposition": "attachment; filename=remote-device.zip",
+        "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0",
+        "Pragma": "no-cache",
+        "Expires": "0"
+    });
+    res.download(fileName);
 });
 
 app.listen(port, () => {
