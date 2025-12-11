febdc9b060c1dcb9c442c2aa4cb0e26387e74d41
Sammy Boy <samueltsutcliffe@gmail.com>
2025-12-11 05:44:19 +0000

    I have changed the vite.config.json file to allow the dev script to host on the local network allow for any device to connect to it

--- emberMetrics/vite.config.js
+++ emberMetrics/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        host: true, // listens on all network interfaces
+        port: 5173, // optional, default is 5173
+    }
 })
