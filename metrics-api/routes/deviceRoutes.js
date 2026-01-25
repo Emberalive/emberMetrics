478e0a8f7b487b4c694bf193dcbc6f1aa344d021
samBook <samueltsutcliffe@gmail.com>
2026-01-25 15:21:35 +0000

    I have deligated the different routes into routers, such as a user route and a device route, seperatng areas of concerns, as well as properly wrote the device routes, i didnt even write them proprly smh

---
+++ metrics-api/routes/deviceRoutes.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const {getDevices, editDevice, addDevice, deleteDevice} = require("../device");
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    console.log("[Server - GET | devices] starting route access")
+    try {
+        const devices = await getDevices()
+        if (!devices) {
+            console.log("[Server - GET | devices] no devices stored on API")
+            res.status(500).json('Devices Data not available')
+        } else {
+            console.log("[Server - GET | devices] devices sent to client")
+            res.status(200).json(devices)
+        }
+    } catch (e) {
+        console.log("[Server - GET | devices] failed")
+        res.status(500).send({
+            success: false,
+        })
+    }
+})
+
+router.patch('/', async (req, res) => {
+    console.log("[Server - PATCH | devices] starting route access")
+    const deviceId = req.params.id
+    if (!deviceId) {
+        console.log("[Server - PATCH | devices] please specify deviceId")
+        res.status(400).send({success: false})
+    } else {
+        try {
+            const response = await editDevice(deviceId)
+            if (response.success === true) {
+                res.status(200).send(response)
+            } else {
+                res.status(500).send(response)
+            }
+        } catch (e) {
+            console.log("[Server - PATCH | devices] internal error:", e)
+            res.status(500).send({success: false})
+        }
+    }
+})
+
+router.post('/', async (req, res) => {
+    console.log("[Server - POST | devices] starting route access")
+    const device = req.body.device
+    if (!device) {
+        console.log("[Server - POST | devices] please specify deviceId")
+        res.status(400).send({success: false})
+    } else {
+        try {
+            const response = await addDevice(req.body.device)
+            if (!response.success) {
+                console.log('[Server - POST | devices] failed to add device')
+                res.status(500).send({
+                    success: false,
+                })
+            } else {
+                console.log('[Server - POST | devices] added device with deviceId:', response.device.id)
+                res.status(200).send(response)
+            }
+        } catch (e) {
+            console.log("[Server - POST | devices] internal error:", e)
+            res.status(500).send({success: false})
+        }
+    }
+})
+
+router.delete('/', async (req, res) => {
+    const deviceId = req.body.device
+    if (!deviceId) {
+        res.status(400).send({
+            success: false,
+        })
+    } else {
+        try {
+            const response = await deleteDevice(deviceId)
+            if (response.success) {
+                res.status(200).json(response)
+            } else {
+                res.status(500).json({
+                    success: false,
+                })
+            }
+        } catch (e) {
+            console.log('[Server - DELETE | devices] internal error:', e)
+            res.status(500).send({
+                success: false,
+            })
+        }
+    }
+})
+
+module.exports = router
---
+++ metrics-api/routes/userRoutes.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const {createUser, deleteUser, authenticateUser} = require('../user')
+const router = express.Router()
+
+router.get('/', async(req, res) => {
+    console.log('[Server - GET /users] starting route access')
+    const userName = req.user.username
+    if (!userName) {
+        console.log('[Server - GET /users] No username sent')
+        return res.status(400).send({
+            error: 'User name is required',
+            success: false
+        })
+    } else {
+        const response = await authenticateUser(userName)
+        if (!response.success) {
+            console.log('[Server - GET /users] authenticateUser failed.]')
+            return res.status(404).send({
+                error: 'User not found',
+                success: false
+            })
+        } else {
+            console.log('[Server - GET /users] authenticateUser succeeded.')
+            res.status(200).send(response)
+        }
+    }
+})
+
+router.post('/', async (req, res) => {
+    console.log('[Server - POST /users] starting route access')
+    const { username, password } = req.body
+    if (!username || !password) {
+        return res.status(400).send({
+            error: 'Username and password required',
+            success: false
+        })
+    } else {
+        try {
+            const response = await createUser(username, password)
+            if (!response.success) {
+                console.log('[Server - POST /users] create user failed.')
+                return res.status(400).send({
+                    success: false,
+                })
+            } else {
+                console.log('[Server - POST /users] create user succeeded.')
+                res.status(200).send(response)
+            }
+        } catch (e) {
+            console.log('[Server - POST /users] internal error:', e)
+            return res.status(500).send({
+                success: false,
+            })
+        }
+    }
+})
+
+router.delete('/', async (req, res) => {
+    console.log('[Server - DELETE /users] starting route access')
+    const { username } = req.body.user
+    if (!username) {
+        console.log('[Server - DELETE /users] no username sent')
+        return res.status(400).send({
+            success: false,
+        })
+    } else {
+        try {
+            const response = await deleteUser(username)
+            if (!response.success) {
+                console.log('[Server - DELETE /users] delete user failed.')
+                return res.status(500).send({
+                    success: false,
+                })
+            } else {
+                console.log('[Server - DELETE /users] delete succeeded.')
+                res.status(200).send(response)
+            }
+        } catch (e) {
+            console.log('[Server - DELETE /users] delete failed.')
+            return res.status(500).send({
+                success: false,
+            })
+        }
+    }
+})
+
+module.exports = router
