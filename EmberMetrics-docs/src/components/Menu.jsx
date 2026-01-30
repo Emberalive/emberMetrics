1517cdb16213c17811cc3764bac3be8217174a49
Sammy Boy <samueltsutcliffe@gmail.com>
2026-01-30 22:47:45 +0000

    I have added basically nothing judt navigation menu as well as the title, and some basic generic styling and the darkMode control

---
+++ EmberMetrics-docs/src/components/Menu.jsx
@@ -0,0 +1,15 @@
+export default function Menu () {
+    return (
+        <section>
+            <header>
+                <h1>Ember Metrics - Docs</h1>
+            </header>
+            <div className={'navigation'}>
+                <p className={'navigation-item'}>Menu-Item</p>
+                <p className={'navigation-item'}>Menu-Item</p>
+                <p className={'navigation-item'}>Menu-Item</p>
+                <p className={'navigation-item'}>Menu-Item</p>
+            </div>
+        </section>
+    )
+}
