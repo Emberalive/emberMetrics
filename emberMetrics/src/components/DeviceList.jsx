b59c63348fafd1e73cbf49be0400316cde73c29b
samBook <samueltsutcliffe@gmail.com>
2026-02-21 12:13:46 +0000

    I have somewhat fixed the issue with the device and user changes when adding, deleting, and updating a device, however when spammed, the server refuses requests after a certain point

--- emberMetrics/src/components/DeviceList.jsx
+++ emberMetrics/src/components/DeviceList.jsx
@@ -52,70 +52,17 @@ export default function DeviceList (props) {
             props.handleNotification('error', 'Error deleting device');
         }
     }
-    // async function deleteDevice (device) {
-    //     try {
-    //         const deviceID = device.id
-    //         //making the request to delete the device
-    //         const response = await fetch(`http://${props.deviceType === 'remote-access' ? props.hostIp : '127.0.0.1'}:3000/devices`, {
-    //             method: "DELETE",
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             },
-    //             body: JSON.stringify({deviceID: deviceID}),
-    //         })
-    //         if (response.ok) {
-    //             //if response status is 200
-    //             const index = props.devices.findIndex(device => device.id === deviceID);
-    //             if (index !== -1) {
-    //                 const updatedDevices = [...props.user.devices].filter(d => d.id !== deviceID);
-    //                 //get the new user data to update the user object
-    //                 const userData = { ...props.user, devices: updatedDevices };
-    //                 //making the request from a global function in App.jsx
-    //                 const response1 = await props.patchUser(userData);
-    //
-    //                 if (response1.success) {
-    //                     //if the function call is accurate then change the user state data
-    //                     props.setUser(response1.updatedUser);
-    //                 } else {
-    //                     //if not send a notification and add the device back to the main json
-    //                     const response2 = await fetch (`http://${props.deviceType === 'remote-access' ? props.hostIp : '127.0.0.1'}:3000/devices`, {
-    //                         method: "POST",
-    //                         headers: {
-    //                             "Content-Type": "application/json"
-    //                         },
-    //                         body: JSON.stringify({ device: deleteDeviceData })
-    //                     });
-    //                     if (response2.ok) {
-    //                         const resData = await response2.json();
-    //                         if (resData.success) {
-    //                             props.handleNotification('error', 'Failed to update device');
-    //                         }
-    //                     }
-    //                 }
-    //                 props.handleNotification('notice', 'Successfully deleted devices');
-    //             }
-    //         } else {
-    //             //if not 200 throw an error
-    //             props.handleNotification('error', 'Failed to delete device');
-    //         }
-    //         setDeleteDeviceData(null);
-    //     } catch (e) {
-    //         props.handleNotification('error', 'Sorry there was an issue deleting this device');
-    //         console.error("[Client - delete device] Failed to retrieve devices", e.message);
-    //     }
-    // }
-
 
     async function patchDevice(e) {
         e.preventDefault();
-        try {
             const originalDevice = props.user.devices.find(d => d.id === editDevice.id);
 
+        try {
             // PATCH the device on the server
             const response = await fetch(`http://${props.deviceType === 'remote-access' ? props.hostIp : '127.0.0.1'}:3000/devices`, {
                 method: 'PATCH',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ device: editDevice }),
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({device: editDevice}),
             });
 
             if (!response.ok) {
@@ -129,21 +76,34 @@ export default function DeviceList (props) {
                 props.handleNotification('error', 'Device update failed on server');
                 return;
             }
-
+        } catch {
+            props.handleNotification('notice', 'Failed to update device');
+            return;
+        }
             // Build new devices array using latest user state
             const newDevices = props.user.devices.map(d =>
                 d.id === editDevice.id ? { ...d, ip: editDevice.ip, name: editDevice.name } : d
             );
-
+            //build new user object with the new devices
             const userData = { ...props.user, devices: newDevices };
 
-            // PATCH user with updated devices
-            const patchResponse = await props.patchUser(userData);
+            //flag to see if the device rollback is needed
+            let userPatchSucceeded = false;
 
-            if (patchResponse.success) {
-                props.setUser(patchResponse.updatedUser);
-                props.handleNotification('notice', 'Device has been updated');
-            } else {
+            try {
+                // PATCH user with updated devices
+                const patchResponse = await props.patchUser(userData);
+
+                if (patchResponse?.success) {
+                    userPatchSucceeded = true;
+                    props.setUser(patchResponse.updatedUser);
+                    props.handleNotification('notice', 'Device has been updated');
+                }
+            } catch {
+                props.handleNotification('error', 'Failed to update device on the user object');
+            }
+
+            if (!userPatchSucceeded) {
                 // rollback device PATCH if user PATCH fails
                 await fetch(`http://${props.deviceType === 'remote-access' ? props.hostIp : '127.0.0.1'}:3000/devices`, {
                     method: 'PATCH',
@@ -152,77 +112,8 @@ export default function DeviceList (props) {
                 });
                 props.handleNotification('error', 'Failed to update user; device rollback applied');
             }
-
-            setEditID(null);
-
-        } catch (e) {
-            console.error('Error in patchDevice:', e);
-            props.handleNotification('error', 'Could not edit your device');
-        }
+        setEditID(null);
     }
-    // async function patchDevice (e) {
-    //     e.preventDefault();
-    //     try {
-    //         //store the original device
-    //         const originalDevice = editDevice;
-    //         // make the request to edit the device
-    //         const response = await fetch(`http://${props.deviceType === 'remote-access' ? props.hostIp : '127.0.0.1'}:3000/devices`, {
-    //             method: 'PATCH',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify({device: editDevice}),
-    //         })
-    //         if (response.ok) {
-    //             const resData = await response.json();
-    //             if (resData.success) {
-    //                 //if there is response data create a new array of devices with the updated device
-    //                 const newDevices = props.devices.map(device => {
-    //                     if (device.id === editDevice.id) {
-    //                         return {...device, ip: editDevice.ip, name: editDevice.name };
-    //                     } else {
-    //                         return device;
-    //                     }
-    //                 })
-    //                 //check that the correct number of devices is returned
-    //                 if (newDevices.length === props.devices.length) {
-    //                     // update the users devices
-    //                     const UserData = {...props.user, devices: newDevices};
-    //                     const response1 = await props.patchUser(UserData);
-    //                     if (response1.success) {
-    //                         //if patchUser was successful then set the userState with the new user data
-    //                         props.handleNotification('notice', 'Device has been updated')
-    //                         props.setUser(response1.updatedUser);
-    //                     } else {
-    //                         //if it didn't work, set the device data back to what it was originally
-    //                         const response2 = await fetch (`http://${props.deviceType === 'remote-access' ? props.hostIp : '127.0.0.1'}:3000/devices`, {
-    //                             method: 'PATCH',
-    //                             headers: {
-    //                                 'Content-Type': 'application/json',
-    //                             },
-    //                             body: JSON.stringify({device: originalDevice}),
-    //                         });
-    //                         if (response2.ok) {
-    //                             //if the changed data has been successful state that there was an error making the changes
-    //                             const resData = await response2.json();
-    //                             if (resData.success) {
-    //                                 props.handleNotification('error', 'Device has not been updated: server error')
-    //                             }
-    //                         }
-    //                     }
-    //                 } else {
-    //                     console.error('New devices is not the same length as devices')
-    //                 }
-    //             } else {
-    //                 props.handleNotification('error', 'Could not edit the device')
-    //             }
-    //         }
-    //         setEditID(null);
-    //         } catch (e) {
-    //         console.error('Error editing a patch', e.message);
-    //         props.handleNotification('error', 'Could not edit your device, sorry');
-    //     }
-    // }
 
     if (props.user.devices) {
         devicesList = props.user.devices.map(device => {
