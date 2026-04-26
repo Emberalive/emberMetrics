fbf559ae869e46e8510a1b45af12fbed3dfc5f02
Sammy Boy <samueltsutcliffe@gmail.com>
2026-04-26 23:44:01 +0100

    Fixing the user profile patch using the wrong url for the server

--- emberMetrics/src/components/user-auth/Profile.jsx
+++ emberMetrics/src/components/user-auth/Profile.jsx
@@ -41,7 +41,7 @@ export default function Profile (props) {
                 props.handleNotification('notice', 'Your session has ran out, please refresh the page');
             }
             console.info('[ App.jsx - patchUser ] starting function')
-            const response = await fetch(`http://${props.deviceType === "remote-access" ? props.hostIp : "127.0.0.1"}:3000/users`, {
+            const response = await fetch(`https://metrics.emberalive.com/users`, {
                 method: 'PATCH',
                 headers: {
                     'Content-Type': 'application/json',
